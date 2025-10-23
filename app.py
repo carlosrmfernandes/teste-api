fdb13fa726122c2f89454d50c09a3b13b93d6170
Carlos Rafael Magalhães Fernandes <Carlos.Fernandes@salas.aulas>
2025-10-22 21:45:03 -0300

    ok

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    data = {
+            "message": "Hello from Flask!",
+            "status": "success"
+    }
+    return data
